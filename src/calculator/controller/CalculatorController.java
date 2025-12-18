20d44816a2f3e3d8079a832eb03d15aa1c4b415b
Penny1990 <stefanie.riewoldt96@gmail.com>
2025-12-18 10:48:13 +0100

    update developController2

--- src/calculator/controller/CalculatorController.java
+++ src/calculator/controller/CalculatorController.java
@@ -1,11 +1,15 @@
 package calculator.controller;
 
-import calculator.model.CalculatorModel;
+import calculator.model.*;
 import calculator.view.CalculatorView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculatorController {
     private CalculatorView calculatorView;
     private CalculatorModel calculatorModel;
+    private double firstNumber;
 
     public CalculatorController(CalculatorView calculatorView, CalculatorModel calculatorModel) {
         this.calculatorView = calculatorView;
@@ -15,11 +19,11 @@ public class CalculatorController {
     private double calculateResult() {
         double result = 0.0;
         double secondNumber;
+
         try {
-            // TODO: die zweite Zahl aus View auslesen
-
-            // TODO: Model auffordern mit der Zahl zu rechnen
-
+            secondNumber = Double.parseDouble(calculatorView.getInputFromDisplay());  // Zweite Zahl auslesen
+            calculatorView.clearDisplay();  // Display leeren
+            result = calculatorModel.calculateResult(firstNumber,secondNumber);  // Mit Zahl rechnen
         } catch(ArithmeticException e) {
             calculatorView.showErrorMessage(e.getMessage());
         } catch(NumberFormatException e) {
@@ -28,5 +32,33 @@ public class CalculatorController {
             calculatorView.showErrorMessage(e.getMessage());
         }
         return result;
+    }
+
+    private class ArithmeticButtonClickListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            // Aufschrift des geklickten Buttons holen
+            if(event.getActionCommand().equals("=")) {  // wenn '='
+                double result = calculateResult();  // -> rechne mit 'calculateResult' und speicher in 'result'
+                calculatorView.writeToDisplay(String.valueOf(result));  // Ergebnis ins Display schreiben
+            }
+
+            String input = calculatorView.getInputFromDisplay();
+
+            try {
+                firstNumber = Double.parseDouble(input);
+                calculatorView.clearDisplay();
+            } catch(NumberFormatException e) {
+                calculatorView.showErrorMessage(e.getMessage());
+            }
+
+            switch(event.getActionCommand()) {
+                case "+" -> calculatorModel.setArithmeticStrategy(new AdditionStrategy());
+                case "-" -> calculatorModel.setArithmeticStrategy(new SubtractionStrategy());
+                case "*" -> calculatorModel.setArithmeticStrategy(new MultiplicationStrategy());
+                case "/" -> calculatorModel.setArithmeticStrategy(new DivisionStrategy());
+
+            }
+        }
     }
 }
--- src/calculator/model/MultiplicationStrategy.java
+++ src/calculator/model/MultiplicationStrategy.java
@@ -1,6 +1,6 @@
 package calculator.model;
 
-public class MultiplyStrategy implements IArithmeticStrategy {
+public class MultiplicationStrategy implements IArithmeticStrategy {
     @Override
     public double calculate(double firstNumber, double secondNumber) {
         return firstNumber * secondNumber;
